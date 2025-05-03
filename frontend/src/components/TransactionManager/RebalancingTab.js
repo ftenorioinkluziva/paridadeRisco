3f62966620fb8a3418f9991c870c476e1b6232aa
Fernando Tenorio <fernando.tenorio@inkluziva.org>
2025-05-02 21:22:03 -0300

    Changes to be committed: 	modified:   frontend/src/components/TransactionManager/RebalancingTab.js

--- frontend/src/components/TransactionManager/RebalancingTab.js
+++ frontend/src/components/TransactionManager/RebalancingTab.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { usePortfolio } from '../../contexts/PortfolioContext';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://fb89d827e7a0.ngrok.app/api';
 
 const RebalancingTab = ({ showSuccessMessage, showErrorMessage }) => {
   const {
