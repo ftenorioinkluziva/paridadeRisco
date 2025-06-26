01a19493d12f7f9350f9aae83536e31ef342b6ad
Fernando Tenorio <fernando.tenorio@inkluziva.org>
2025-06-26 14:38:55 -0300

    CORS

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ allowed_origins = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
-    "http://127.0.0.1:5001",
+    "http://127.0.0.1:5001/api",
     
     # Cloudflare Tunnels
     "https://riskyparity.blackboxinovacao.com.br",
